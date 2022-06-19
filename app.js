91062dc0ac2292cbf94e2176fa1e0207e95ae85c
Anna Litovskaya <alitovskaya@alitovskaya-mac.local>
2022-06-19 00:13:13 -0400

    obj, arrays, enums

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Your code goes here...');
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[]
+// } = {
+var person = {
+    name: 'Maximilian',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+};
+var favoriteActivities;
+favoriteActivities = ['Sports'];
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
