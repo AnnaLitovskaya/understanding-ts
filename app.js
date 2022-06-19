303cb2bb116030305ca1022ecc492340b2c70b2b
Anna Litovskaya <alitovskaya@alitovskaya-mac.local>
2022-06-19 18:05:28 -0400

    section 2 complete

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[]
-// } = {
-var person = {
-    name: 'Maximilian',
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
-};
-var favoriteActivities;
-favoriteActivities = ['Sports'];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Max';
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError('An error occurred!', 500);
+console.log(result); // nothing cause an error was thrown
